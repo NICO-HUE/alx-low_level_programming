eecf45da3aeb9f101f7077e76caa926391d13230
vigan_nicole <midokpe12@gmail.com>
2023-04-22 20:29:35 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
  * Return: Always 0 (Success)
  */
 
-int main (void)
+int main(void)
 
 {
 	char low;
 
-	for(low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= 'z'; low++)
 		putchar(low);
 	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
