acafbc74ad68655ac07c2a9b101e65f85aae7788
vigan_nicole <midokpe12@gmail.com>
2023-04-27 14:12:48 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,7 @@
  * add - Adds two values
  * @n1: The first number to be computed
  * @n2: The second number to be comted
- * Retur: Igrer value
+ * Return: Igrer value
 */
 
 int add(int n1, int n2)
--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -4,8 +4,7 @@
  * main - check the code.
  *
  * Return: Always 0.
-*/
-
+ */
 int main(void)
 {
 	print_times_table(3);
@@ -14,6 +13,6 @@ int main(void)
 	_putchar('\n');
 	print_times_table(98);
 	_putchar('\n');
-	print_times_table(12);
+	print_times_table(12);  
 	return (0);
 }
